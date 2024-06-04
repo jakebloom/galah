0992efa1101c5e9c880667e9dca05c398e96d710
Jake Bloom <jake@jakebloom.me>
2024-06-04 11:36:35 -0700

    Thank you page is almost done

---
+++ thanks/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(openPopup, 1500);
+})
+
+
+function openPopup() {
+  // document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
